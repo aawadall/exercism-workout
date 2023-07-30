f4d9c95045b7388379bf460a2c4876282b0f2ad3
Ahmed Awadallah <aawadall@ualberta.ca>
2023-07-30 19:04:55 -0400

    minor refactoring

--- java/elons-toy-car/src/main/java/ElonsToyCar.java
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -2,6 +2,10 @@ public class ElonsToyCar {
     private int distanceDriven = 0;
     private int batteryRemaining = 100;
 
+    // magic numbers 
+    private static final int BATTERY_DECAY_PER_DRIVE = 1;
+    private static final int DISTANCE_PER_DRIVE = 20;
+    private static final int LOW_BATTERY_THRESHOLD = 0;
     public static ElonsToyCar buy() {
         return new ElonsToyCar();
     }
@@ -11,7 +15,7 @@ public class ElonsToyCar {
     }
 
     public String batteryDisplay() {
-        var display = batteryRemaining > 0 ? String.format("at %d%%", batteryRemaining) : "empty";
+        var display = batteryRemaining > LOW_BATTERY_THRESHOLD ? String.format("at %d%%", batteryRemaining) : "empty";
         return String.format("Battery %s", display);
     }
 
@@ -21,10 +25,10 @@ public class ElonsToyCar {
     }
 
     private void updateDistance() {
-        distanceDriven += this.batteryRemaining > 0 ? 20 : 0;
+        distanceDriven += this.batteryRemaining > LOW_BATTERY_THRESHOLD ? DISTANCE_PER_DRIVE : 0;
     }
 
     private void updateBattery() {
-        batteryRemaining -= 1;
+        batteryRemaining -= BATTERY_DECAY_PER_DRIVE;
     }
 }
