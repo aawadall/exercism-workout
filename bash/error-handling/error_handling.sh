18e78c6d1913592310db6f72caf85af2ac910e69
Ahmed Awadallah <aawadall@ualberta.ca>
2023-08-01 19:21:50 -0400

    more bash

---
+++ bash/error-handling/error_handling.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Usage message
+USAGE="Usage: error_handling.sh <person>"
+
+# if arguments != 1, print usage
+if [[ $# -ne 1 ]]; then
+    echo $USAGE
+    exit 1
+fi
+
+# Name is arg 1
+NAME=$1
+
+# Print hello, name
+echo "Hello, $NAME"
