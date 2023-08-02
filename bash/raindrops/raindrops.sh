8c6ce0e35dc7129269acbf003b19fbb6a2b604d5
Ahmed Awadallah <aawadall@ualberta.ca>
2023-08-02 10:30:21 -0400

    use dictionary like structure

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,52 +1,39 @@
 #!/usr/bin/env bash
 # raindrops exercise solution
 
-# add factor function 
-add_factor() {
-    if (( $1 % $2 == 0 )); then
-        echo "$4$3"
-    else
-        echo "$4"
-    fi
-}
-
-# build factor sound function
-build_factor_sound() {
-    NUMBER=$1
-
-    # magic numbers
-    PLING=3
-    PLANG=5
-    PLONG=7
-
-    # magic words
-    PLING_STR="Pling"
-    PLANG_STR="Plang"
-    PLONG_STR="Plong"
-
-    # main logic
-    SOUND=""
-
-    # check if divisible by 3
-    SOUND="$(add_factor $NUMBER $PLING $PLING_STR $SOUND)"
-    # check if divisible by 5
-    SOUND="$(add_factor $NUMBER $PLANG $PLANG_STR $SOUND)"
-    # check if divisible by 7
-    SOUND="$(add_factor $NUMBER $PLONG $PLONG_STR $SOUND)"
-
-    if [ -z "$SOUND" ]; then
-        SOUND="$NUMBER"
-    fi
-
-    echo "$SOUND"
-}
-
 # short circuit if number of arguments is not 1 
 USAGE_MSG="Usage: $0 <number>"
 if [ $# -ne 1 ]; then
   echo "$USAGE_MSG"
   exit 1
 fi
+
+# globals
+# declare -A sounds
+sounds=()
+sounds[3]="Pling"
+sounds[5]="Plang"
+sounds[7]="Plong"
+
+# add factor function 
+add_factor() {
+    if (( $1 % $2 == 0 )); then
+        echo "$3"
+    fi
+}
+
+# build factor sound function
+build_factor_sound() {
+    number=$1
+
+    sound=""
+
+    for factor in "${!sounds[@]}"; do
+        sound+=$(add_factor "$number" "$factor" "${sounds[$factor]}")
+    done
+
+    echo "${sound:-$number}"
+}
 
 # numeric argument 
 build_factor_sound "$1"
