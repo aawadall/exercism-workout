a3ee297f0c6abfa3c3ccd1fa38b41b917da1e3a1
Ahmed Awadallah <aawadall@ualberta.ca>
2023-08-03 08:58:57 -0400

    using bloom filter

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,31 +1,22 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 class IsogramChecker {
-    private static final List<String> EXCLUDED_CHARS = Arrays.asList("-", " ");
+    private static final List<Character> EXCLUDED_CHARS = Arrays.asList('-', ' ');
 
     boolean isIsogram(String phrase) {
-        
-        // exclude some characters from the string 
-        for (var excludedChar : EXCLUDED_CHARS) {
-            phrase = phrase.replace(excludedChar, "");
-        }
+        int bloomFilter = 0;
 
-        phrase = phrase.toLowerCase().trim(); // compact case and remove spaces
-
-        var charMap = phrase.chars() // break string to int stream
-                    .mapToObj(c -> (char) c) // convert to char
-                    .collect(Collectors.groupingBy( // map to char count
-                        Function.identity(),
-                        Collectors.counting()));
-    
-        // if the map has no char with count > 1, then it is an isogram 
-        return !charMap
-            .keySet()
-            .stream()
-            .anyMatch(c -> charMap.get(c) > 1); // check if any char has count > 1
+        for(var character : phrase.toLowerCase().toCharArray()) {
+            if(!EXCLUDED_CHARS.contains(character)) {
+                int bit = 1 << (character - 'a');
+                if((bloomFilter & bit) != 0) {
+                    return false;
+                }
+                bloomFilter |= bit;
+            }
+        }    
+        return true;
     }
 
 }
