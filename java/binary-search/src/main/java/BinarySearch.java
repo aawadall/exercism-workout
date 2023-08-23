bcb5aacf2c6ffa5c7d7e170f8ee7e3f713bc62a6
Ahmed Awadallah <aawadall@ualberta.ca>
2023-08-23 15:13:27 -0400

    incorrect implementation

---
+++ java/binary-search/src/main/java/BinarySearch.java
@@ -0,0 +1,78 @@
+import java.util.List;
+
+class BinarySearch {
+    private int[] ints;
+
+    public BinarySearch(int[] ints) {
+        this.ints = ints;
+    }
+
+    public BinarySearch(List<Integer> ints) {
+        this.ints = ints.stream().mapToInt(i -> i).toArray();
+    }
+
+    int indexOf(int i) throws ValueNotFoundException{
+        return search(i, ints);
+    }
+
+    int search(int a, int[] arr) throws ValueNotFoundException {
+        Integer[] boxedArr = new Integer[arr.length];
+        
+        return (int) genericSearch(a, boxedArr);
+        
+    }
+
+    int search(float a, float[] arr) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    int search(double a, double[] arr) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    int search(long a, long[] arr) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    /**
+     * Generic binary search
+     * 
+     * @param a   value to search
+     * @param arr sorted array to search in
+     * @param <T> type of array
+     * @return index of value in array
+     */
+    private static <T extends Comparable<T>> int genericSearch(T a, T[] arr) throws ValueNotFoundException{
+        var found = false;
+        var leftIndex = 0;
+        var rightIndex = arr.length - 1;
+
+        while (!found) {
+            // slice array in half
+            var middleIndex = (leftIndex + rightIndex) / 2;
+            var middleValue = arr[middleIndex];
+
+            // check if left and right index have crossed
+            if (leftIndex > rightIndex) {
+                break;
+            }
+
+            // check if middle value is equal to a
+            if (middleValue.compareTo(a) == 0) {
+                return middleIndex;
+            }
+
+            // check if middle value is less than a
+            if (middleValue.compareTo(a) < 0) {
+                leftIndex = middleIndex + 1;
+            }
+
+            // check if middle value is greater than a
+            if (middleValue.compareTo(a) > 0) {
+                rightIndex = middleIndex - 1;
+            }
+        }
+
+        throw new ValueNotFoundException("value not in array");
+    }
+}
