eb6f7336cbf8e4b274d14d6fd3ad83fba666ae03
Ahmed Awadallah <aawadall@ualberta.ca>
2023-08-26 10:13:14 -0400

    still stuck

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -115,5 +115,68 @@ class PigLatinTranslator {
     private static boolean rule3Condition(String word) {
         return !rule1Condition(word) && word.substring(1).toLowerCase().startsWith("qu");
     }
-    // Rule 4: If a word contains a "y" after a consonant cluster or as the second letter in a two letter word it makes a vowel sound (e.g. "rhythm" -> "ythmrhay", "my" -> "ymay").
+    
+    /**
+     * Rule 4
+     * If a word contains a "y" after a consonant cluster 
+     * or as the second letter in a two letter word 
+     * it makes a vowel sound 
+     * (e.g. "rhythm" -> "ythmrhay", "my" -> "ymay").
+     * @param word the word to translate
+     * @return the translated word
+     */
+    private static String rule4(String word) {
+        var result = new StringBuilder(word);
+        // check rule 
+        if (rule4Condition(word)) {
+            // apply rule 
+            result.append(word.charAt(0));
+            result.deleteCharAt(0);
+            result.append("ay");
+        }
+        
+        return result.toString();
+    }
+
+    /**
+     * Rule 4 condition
+     * If a word contains a "y" after a consonant cluster 
+     * or as the second letter in a two letter word
+     * @param word the word to translate
+     * @return true if the word contains a "y" after a consonant cluster 
+     * or as the second letter in a two letter word
+     */
+    private static boolean rule4Condition(String word) {
+        // scan for consonant cluster
+        int index = 0;
+        var consonantCluster = new StringBuilder();
+        for (var i = 0; i < word.length(); i++) {
+            var c = word.charAt(i);
+            index = i;
+            if (isVowel(c)) {
+                break;
+            }
+            consonantCluster.append(c);
+        }
+        // check if index + 1 is y
+        if (index + 1 < word.length() && word.charAt(index + 1) == 'y') {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check if a character is a vowel
+     * @param c the character to check
+     * @return true if the character is a vowel
+     */
+    private static boolean isVowel(char c) {
+        var vowels = new char[] {'a', 'e', 'i', 'o', 'u', 'y'};
+        for (var vowel : vowels) {
+            if (c == vowel) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
